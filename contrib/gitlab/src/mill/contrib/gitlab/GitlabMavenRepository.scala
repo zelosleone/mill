627a95276c05e593c5d79c8fe7201f93d6a25b05
Denizhan Dakılır <denizhandakilirn@gmail.com>
2025-02-22 15:34:37 +0300

    Revert "Gitlab fix!?"
    This reverts commit 1b1ab5c1138d6332b9f3f88e6c7bad8ba315f8e8.

--- contrib/gitlab/src/mill/contrib/gitlab/GitlabMavenRepository.scala
+++ contrib/gitlab/src/mill/contrib/gitlab/GitlabMavenRepository.scala
@@ -5,16 +5,15 @@ import coursier.maven.MavenRepository
 import mill.api.Result
 import mill.api.Result.{Failure, Success}
 import mill.define.Task
-import mill.define.Task.T
 
 trait GitlabMavenRepository {
 
   def tokenLookup: GitlabTokenLookup = new GitlabTokenLookup {} // For token discovery
   def gitlabRepository: GitlabPackageRepository // For package discovery
 
-  def mavenRepository: T[MavenRepository] = T {
+  def mavenRepository: Task[MavenRepository] = Task.Anon {
 
-    val gitlabAuth = tokenLookup.resolveGitlabToken(T.env, sys.props.toMap, T.workspace)
+    val gitlabAuth = tokenLookup.resolveGitlabToken(Task.env, sys.props.toMap, Task.workspace)
       .map(auth => Authentication(auth.headers))
       .map(auth => MavenRepository(gitlabRepository.url(), Some(auth)))
 
